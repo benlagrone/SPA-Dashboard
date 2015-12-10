b0b830a2b0f0483f9fc817ca84bd53c226d3b744
Ben <benjaminlagrone@gmail.com>
2015-12-09 20:26:05 -0600

    added services.routing added services.routing.changeHash

--- app/service.js
+++ app/service.js
@@ -4,4 +4,21 @@ services.getPage = function(url,id,callback,page){
     //Do something
     var xhttp;
     xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            callBack(xhttp,id,page);
+        }
+    };
+    xhttp.open('GET', url, true);
+    xhttp.send();
 };
+
+services.routing = {};
+
+services.routing.changeHash = function(page){
+    var newPageUrl = 'app/'+ page + '/' + page+'.html';
+    var newScriptUrl = 'app/'+ page + '/' + page+'.js';
+    services.getPage(newPageUrl,'content',services.routing.writeHTML,page);
+    services.getPage(newScriptUrl,'head',services.routing.writeScript,page);
+};
+
