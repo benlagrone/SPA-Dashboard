0594afd89d1a4e8f6c36949336103c894184c885
Ben <benjaminlagrone@gmail.com>
2015-12-09 20:52:51 -0600

    added services.routing.writeHTML added services.routing.writeScript

--- app/service.js
+++ app/service.js
@@ -1,7 +1,6 @@
 var services = {};
 
 services.getPage = function(url,id,callback,page){
-    //Do something
     var xhttp;
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -12,13 +11,22 @@ services.getPage = function(url,id,callback,page){
     xhttp.open('GET', url, true);
     xhttp.send();
 };
-
 services.routing = {};
-
 services.routing.changeHash = function(page){
     var newPageUrl = 'app/'+ page + '/' + page+'.html';
     var newScriptUrl = 'app/'+ page + '/' + page+'.js';
     services.getPage(newPageUrl,'content',services.routing.writeHTML,page);
     services.getPage(newScriptUrl,'head',services.routing.writeScript,page);
+};
+
+services.routing.writeHTML = function(xhr,id,page){
+    var theHTML = xhr.responseText;
+    document.getElementById(id).innerHTML = xhr.responseText;
+};
+services.routing.writeScript = function(xhr,id,page){
+    var newScript = document.createElement('script');
+    newScript.text = 'var id= "'+page+'";';
+    newScript.text += xhr.responseText;
+    document.getElementsByTagName(id).item(0).appendChild(newScript);
 };
 
