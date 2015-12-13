c2f0577a1da3717c027b3400e474afbb002706d4
Ben <benjaminlagrone@gmail.com>
2015-12-12 19:56:21 -0600

    rewriting the chapter, loading the AJAX

--- app/service.js
+++ app/service.js
@@ -1,30 +1,22 @@
 var services = {};
-
-services.getPage = function(url,id,callback,page){
+services.getPage = function(url,id,callback){
     var xhttp;
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-            callBack(xhttp,id,page);
+            callback(xhttp,id)
         }
     };
     xhttp.open('GET', url, true);
     xhttp.send();
 };
+
 services.routing = {};
-services.routing.changeHash = function(page){
-    services.getPage(pageRoute.page,'content',services.routing.writeHTML,page);
-    services.getPage(pageRoute.script,'head',services.routing.writeScript,page);
+services.routing.writeHTML = function(xhttp,id){
+    document.getElementById(id).innerHTML = xhttp.responseText;
 };
 
-services.routing.writeHTML = function(xhr,id,page){
-    var theHTML = xhr.responseText;
-    document.getElementById(id).innerHTML = xhr.responseText;
-};
-services.routing.writeScript = function(xhr,id,page){
-    var newScript = document.createElement('script');
-    newScript.text = 'var id= "'+page+'";';
-    newScript.text += xhr.responseText;
-    document.getElementsByTagName(id).item(0).appendChild(newScript);
-};
 
+var url = './app/home/home.html';
+var id = "content";
+services.getPage(url, id, services.routing.writeHTML);
